ff84bcc582fd0fa054d7947df4aba5726f35f252
Max Fernandes de Souza <xaamax@gmail.com>
2025-03-28 23:35:10 -0300

    feature: configurando DatabaseContext

---
+++ backend/src/CursosFormacoes.Persistence/Context/DatabaseContext.cs
@@ -0,0 +1,37 @@
+﻿using CursosFormacoes.Domain.Entities;
+using CursosFormacoes.Domain.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursosFormacoes.Persistence.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Encontra todas as entidades que herdam BaseEntity
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var entity = entityType.ClrType;
+                // Verifica se a entidade herda de BaseEntity
+                if (typeof(BaseEntity).IsAssignableFrom(entity))
+                {
+                    var entityBuilder = modelBuilder.Entity(entity);
+                    // Configura 'created_at' para usar GETDATE() como valor padrão
+                    entityBuilder.Property(nameof(BaseEntity.CreatedAt))
+                        .HasDefaultValueSql("GETDATE()");
+                    // Configura 'inative' para ter false como valor padrão
+                    entityBuilder.Property(nameof(BaseEntity.Inative))
+                        .HasDefaultValue(false);
+                    // 'email' deve ser único
+                    modelBuilder.Entity<Teacher>()
+                        .HasIndex(c => c.Email)
+                        .IsUnique();
+                }
+            }
+        }
+    }
+}
