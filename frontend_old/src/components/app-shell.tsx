4ccc7088a82d7a47c970152d1c7348bef925c17b
Max Fernandes de Souza <xaamax@gmail.com>
2025-04-03 21:25:06 -0300

    refactor: removendo frontend_old

--- frontend_old/src/components/app-shell.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom'
-
-export default function AppShell() {
-  return (
-    <div className='relative h-full overflow-hidden bg-background'>
-        <Outlet />
-    </div>
-  )
-}
